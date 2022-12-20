7ec80edee9795de3f6331532a35cf784e25d035a
Israel Garate <israel.garate@tsoftlatam.com>
2022-12-20 10:30:26 -0300

    bypass linux

--- src/test/java/driverConfig/DriverManager.java
+++ src/test/java/driverConfig/DriverManager.java
@@ -42,7 +42,7 @@ private String extensionDriver = "";
 					chromeOptions.addArguments("--disable-dev-shm-usage");
 					chromeOptions.addArguments("--ignore-certificate-errors");
 					chromeOptions.addArguments("--disable-extensions");
-
+					chromeOptions.addArguments("--remote-debugging-port=9222");
 					chromeOptions.addArguments("--disable-gpu");
 					chromeOptions.addArguments("--no-sandbox");
 					chromeOptions.addArguments("window-size=1920,1080");
