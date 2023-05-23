41450b7ee8320cf715e8f0959c4d45986a09b45c
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-05-23 14:06:44 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -11,10 +11,10 @@ echo "ID de ejecucion: $TESTEXECUTION"
 echo "Attachment para ejecucion"
 #export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/$TESTEXECUTION/attachments
 export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/XRAY-18/attachments
-export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/XRAY-20/attachments
 echo $url
 cd results/
 pwd
 echo $url>url.txt
+cat $url
 #curl -H \'Authorization: Basic YWFyb24uYmFycmFAdHNvZnRsYXRhbS5jb206QVRBVFQzeEZmR0YwUlVfeHdnZHdLRGRfZFlxU1VnamgxVkpaM2VjQ2hlUzdZVU5oSlhlRDdIVjlIYVc4TmRNaDN4S2VXTEh1aHAzODViOE41V1k5VVhJUWhyQnF6WmZIQnloamlzNFBTbXcyUEJqSlNGUE12N2RZVHRobC1Edlg2WklUZHdHRTNkRk9MbjRLN2dfVkkzS1k5VU1BTzJ4dTdSRnYwUjRnOFZkQ1dXRzI0WTN4eHRjPUVCM0UwMTU4' -X POST $tempUrl -H \"Content-Type:multipart/form-data\" -H \'X-Atlassian-Token: no-check\' -F \'file=@"reporteQA.pdf\"\'
 curl -H 'Authorization: Basic YWFyb24uYmFycmFAdHNvZnRsYXRhbS5jb206QVRBVFQzeEZmR0YwUlVfeHdnZHdLRGRfZFlxU1VnamgxVkpaM2VjQ2hlUzdZVU5oSlhlRDdIVjlIYVc4TmRNaDN4S2VXTEh1aHAzODViOE41V1k5VVhJUWhyQnF6WmZIQnloamlzNFBTbXcyUEJqSlNGUE12N2RZVHRobC1Edlg2WklUZHdHRTNkRk9MbjRLN2dfVkkzS1k5VU1BTzJ4dTdSRnYwUjRnOFZkQ1dXRzI0WTN4eHRjPUVCM0UwMTU4' -X POST "$url" -H "Content-Type:multipart/form-data" -H 'X-Atlassian-Token: no-check' -F 'file=@"reporteQA.pdf"'
