46b497fcb6f597a258457561d186975ccf8d237b
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-05-23 12:05:55 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Generando token"
-export token=$(curl -H "Content-Type: application/json" -X POST --data @"infra/cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
+export token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
 echo $token
 echo "Token Generado"
