68969859c8ab1b26d38a98b93f82c113f0b256a7
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-05-23 12:03:39 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,4 +1,5 @@
-sh '''
-          token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
-          curl -H "Content-Type: multipart/form-data" -X POST -F info=@Info.json -F results=@src/test/resources/output/report/Cucumber.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
-          '''
+#!/bin/bash
+echo "Generando token"
+export token=$(curl -H "Content-Type: application/json" -X POST --data @"infra/cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
+echo $token
+echo "Token Generado"
