dbaaee36498bad9857b339569cc91045c5a1b8c7
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-05-29 15:27:40 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -8,7 +8,7 @@ echo "Token Generado"
 echo "Cargando TestExecution"
 curl -H "Authorization: Bearer $token" -X POST 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' -H "Content-Type:multipart/form-data" -F 'info=@"Info.json"' -F 'results=@"results/Cucumber.json"' > log.txt
 cat log.txt
-export TESTEXECUTION=$(grep "XRAY-" log.txt | cut -d '"' -f 8)
+export TESTEXECUTION=$(grep "PX-" log.txt | cut -d '"' -f 8)
 echo "ID de ejecucion: ${TESTEXECUTION}"
 echo "Attachment para ejecucion"
 export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/${TESTEXECUTION}/attachments
