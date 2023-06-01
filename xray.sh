21dcda2e4a0d29b8bdd84aacc538d456b703e25c
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-01 11:17:22 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "archivos antes de crear token"
-ls -ls
 echo "Generando token"
 export token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
 echo $token
 echo "Token Generado"
+echo "Personalizacion Jira"
+sed -i 's/"summary": "a"/"summary": "Resultado Testing Continuo GIT "/g' integracion/jira.json
 echo "Cargando TestExecution"
 curl -H "Authorization: Bearer $token" -X POST 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' -H "Content-Type:multipart/form-data" -F 'info=@"Info.json"' -F 'results=@"results/Cucumber.json"' > log.txt
 cat log.txt
@@ -12,15 +13,8 @@ export TESTEXECUTION=$(grep "PX-" log.txt | cut -d '"' -f 8)
 echo "ID de ejecucion: ${TESTEXECUTION}"
 echo "Attachment para ejecucion"
 export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/${TESTEXECUTION}/attachments
-#export url=https://capacitacionxray9.atlassian.net/rest/api/3/issue/XRAY-18/attachments
 echo "VERIFICANDO URL"
 echo $url
-echo "archivos antes de ir a la carpeta results"
-ls -ls
-echo "ACCESO A CARPETA RESULTS"
 cd results/
-echo "archivos carpeta results"
-ls -ls
-pwd
 echo "ENVIO DE ARCHIVO"
 curl -H 'Authorization: Basic YWFyb24uYmFycmFAdHNvZnRsYXRhbS5jb206QVRBVFQzeEZmR0YwWFZLWl91NldZU2ltOEZ0NjFQWTZvZTB5WWlGejNRQkloeGtRZ0NJdlFCNkktdXpRdHZoTnpWWDQtM3BvVEtIODhqLXdFdmF3NFdDVjdNbzkwTDQ5OXRjNEVlcmxzM3oyWmxDTS1mMmpDckQ0WFRFeXB1bmJuOXRTYzRQcFIwNFlBcG9yXzRxWmZBNFR0WjVhWWRrOHNmajZnLTVzYlA0UkJ0VWZrWTZxc0hzPTFGN0IzMEVG' -X POST $url -H "Content-Type:multipart/form-data" -H 'X-Atlassian-Token: no-check' -F file=@'reporteQA.pdf'
