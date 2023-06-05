c3f3189ff64713c96854f7d57f797bcd614f4c8b
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-06-05 11:46:24 -0400

    Update testgradle.sh

--- testgradle.sh
+++ testgradle.sh
@@ -6,7 +6,12 @@ cat "IMPRIMIENDO RESULTADO LOG"
 cat logGradle.txt
  
 if grep -q "BUILD SUCCESSFUL" logGradle.txt; then
-  echo "SUCCESS"
+  echo "Repositorio clonado con éxito"
+sed -i 's/"summary": "Clonacion exitosa"/"summary": "Ejecucion exitosa"/g' integracion/jira.json
+curl -H "Content-Type: multipart/form-data" -X POST -F info=@integracion/jira.json -F results=@integracion/prueba-passed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
 else
-  echo "FAILURE"
+  echo "FALLO"
+  sed -i 's/"summary": "Clonacion exitosa"/"summary": "Ejecucion fallida"/g' integracion/jira.json
+  curl -H "Content-Type: multipart/form-data" -X POST -F info=@integracion/jira.json  -F results=@integracion/prueba-failed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart    
+  exit 1
 fi
