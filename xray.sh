55b73a946cf19acacaa538b5ad478f882d9487c9
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-06-05 17:48:50 -0400

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -6,19 +6,18 @@ echo $token
 echo "Token Generado"
 echo "Personalizacion Jira"
 if [ $TIPO -eq 1 ]; then
-  sed -i 's/"summary": "Clonacion exitosa"/"summary": "Resultado Prueba Automatizada ON-DEMAND "/g' integracion/jira.json
+  sed -i 's/"summary": "TITULOJSON"/"summary": "Resultado Prueba Automatizada ON-DEMAND "/g' integracion/jira.json
 else
-  sed -i 's/"summary": "Clonacion exitosa"/"summary": "Resultado Prueba Automatizada PIPELINE "/g' integracion/jira.json
+  sed -i 's/"summary": "TITULOJSON"/"summary": "Resultado Prueba Automatizada PIPELINE "/g' integracion/jira.json
 fi
 
 if grep -q "BUILD SUCCESSFUL" logGradle.txt; then
   echo "PASO"
-  sed -i 's/"description": "Clonacion exitosa"/"description": "Ejecucion exitosa. [~accountid:63d93e141b13d42998e0e160][~accountid:610ab6d00b454a00681fbd5e]"/g' integracion/jira.json
+  sed -i 's/"description": "Fallo al clonar"/"description": "Ejecucion exitosa. [~accountid:63d93e141b13d42998e0e160][~accountid:610ab6d00b454a00681fbd5e]"/g' integracion/jira.json
 else
   echo "FALLO"
-  sed -i 's/"description": "Clonacion exitosa"/"description": "Ejecución fallida. [~accountid:63d93e141b13d42998e0e160][~accountid:610ab6d00b454a00681fbd5e]"/g' integracion/jira.json
+  sed -i 's/"description": "Fallo al clonar"/"description": "Ejecución fallida. [~accountid:63d93e141b13d42998e0e160][~accountid:610ab6d00b454a00681fbd5e]"/g' integracion/jira.json
 fi
-sed -i 's/"description": "Clonacion exitosa"/"description": "Ejecución fallida. [~accountid:63d93e141b13d42998e0e160][~accountid:610ab6d00b454a00681fbd5e]"/g' integracion/jira.json
 echo "Cargando TestExecution"
 curl -H "Authorization: Bearer $token" -X POST 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' -H "Content-Type:multipart/form-data" -F 'info=@"integracion/jira.json"' -F 'results=@"results/Cucumber.json"' > log.txt
 cat log.txt
