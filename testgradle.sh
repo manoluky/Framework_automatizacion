02563ff6492d90e40f2948b9221a1e9635720f90
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-27 11:40:46 -0400

    Delete testgradle.sh

--- testgradle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TAG=$1
-xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -P tags=$TAG ; exit 0 > logGradle.txt
-
-cat "IMPRIMIENDO RESULTADO LOG"
-cat logGradle.txt
-
